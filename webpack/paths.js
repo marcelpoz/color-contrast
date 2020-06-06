89220ca713a6cd93c47cf5b64586f244b1aaccf4
Marcel <ikbenmarcelpos@gmail.com>
2020-06-07 00:10:04 +0200

    🔧 Added webpack paths helper

---
+++ webpack/paths.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
+
+const paths = {
+  root: path.resolve(__dirname, '../'),
+  src: path.resolve(__dirname, '../src'),
+  dist: path.resolve(__dirname, '../dist'),
+};
+
+module.exports = paths;
