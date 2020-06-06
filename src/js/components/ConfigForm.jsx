55881c09b958f2b9e403924e607b73d1c0206946
Marcel <ikbenmarcelpos@gmail.com>
2020-06-07 00:14:53 +0200

    🔨 Added ConfigForm component

---
+++ src/js/components/ConfigForm.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+
+import Context from '../Context';
+
+import ColorPicker from './ColorPicker';
+import InputFontSize from './InputFontSize';
+import SelectFontWeight from './SelectFontWeight';
+
+export default () => {
+  const context = useContext(Context);
+  const { bgColour, textColour } = context;
+
+  return (
+    <form
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+      }}
+    >
+      <ColorPicker type="textColour" color={textColour} title="Text Colour" />
+      <ColorPicker type="bgColour" color={bgColour} title="Background Colour" />
+      <fieldset>
+        <InputFontSize />
+        <SelectFontWeight />
+      </fieldset>
+    </form>
+  );
+};
