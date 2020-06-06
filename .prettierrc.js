74d52f2d65cdd2844ef5511fc949b00d4781a1f7
Marcel <ikbenmarcelpos@gmail.com>
2020-06-07 00:10:51 +0200

    🔧 Added prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  singleQuote: true,
+  printWidth: 100,
+  semi: true,
+  trailingComma: 'es5',
+  useTabs: false,
+  tabWidth: 2,
+};
