3223e31c34fa95b37a86b4db747539d5b0324ff1
Marcel <ikbenmarcelpos@gmail.com>
2020-06-07 00:14:44 +0200

    🔨 Added Preview component

---
+++ src/js/components/Preview.jsx
@@ -0,0 +1,39 @@
+import React, { useContext } from 'react';
+import Context from '../Context';
+
+const Preview = () => {
+  const context = useContext(Context);
+  const { bgColour, fontSize, fontWeight, textColour } = context;
+  return (
+    <section
+      style={{
+        position: 'relative',
+        display: 'flex',
+        flexFlow: 'column nowrap',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: bgColour || '#FFFFFF',
+        flexGrow: 1,
+      }}
+    >
+      <div
+        style={{
+          display: 'block',
+          width: '100%',
+          height: '100%',
+          padding: '3.6rem',
+          textAlign: 'center',
+          background: 'transparent',
+          border: 0,
+          color: textColour || '#000000',
+          fontSize: fontSize || 16,
+          fontWeight: fontWeight || 400,
+        }}
+      >
+        <span contentEditable="true">Colour Contrast Checker</span>
+      </div>
+    </section>
+  );
+};
+
+export default Preview;
