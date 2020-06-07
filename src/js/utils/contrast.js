9bd591da8fc5689a37457e97f009687e319f91d7
Marcel <ikbenmarcelpos@gmail.com>
2020-06-07 15:29:23 +0200

    🔨 Add some validation to contrast util

--- src/js/utils/contrast.js
+++ src/js/utils/contrast.js
@@ -1,8 +1,20 @@
 import luminance from './luminance';
 
 export default (frontRgb, backRgb) => {
-  const lFront = luminance(frontRgb[0], frontRgb[1], frontRgb[2]);
-  const lBack = luminance(backRgb[0], backRgb[1], backRgb[2]);
+  if (
+    !frontRgb ||
+    !backRgb ||
+    !frontRgb.r ||
+    !frontRgb.g ||
+    !frontRgb.b ||
+    !backRgb.r ||
+    !backRgb.g ||
+    !backRgb.b
+  ) {
+    return false;
+  }
 
+  const lFront = luminance(frontRgb.r, frontRgb.g, frontRgb.b);
+  const lBack = luminance(backRgb.r, backRgb.g, backRgb.b);
   return lBack > lFront ? lFront / lBack : lBack / lFront;
 };
