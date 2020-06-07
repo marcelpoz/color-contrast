ee6d25baa84dcd34cb80458164efc7e414e9ed2b
Marcel <ikbenmarcelpos@gmail.com>
2020-06-07 15:23:31 +0200

    🔨 Moved results component to main app component

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
 import { ContextProvider } from './Context';
+import Form from './components/Form';
 import Preview from './components/Preview';
-import ConfigForm from './components/ConfigForm';
+import Results from './components/Results';
 
 export default () => {
   return (
     <ContextProvider>
       <main role="main" style={{ display: 'flex', flexFlow: 'column nowrap', minHeight: '100vh' }}>
+        <Form />
+        <Results />
         <Preview />
-        <ConfigForm />
       </main>
     </ContextProvider>
   );
