922c6cedfbaacbeb3a83605903a46e6a6f35d2d4
Marcel <ikbenmarcelpos@gmail.com>
2020-06-07 15:24:55 +0200

    🔨 Added separate displayValue for input font size control

--- src/js/components/InputFontSize.jsx
+++ src/js/components/InputFontSize.jsx
@@ -3,20 +3,56 @@ import React, { Component } from 'react';
 import Context from '../Context';
 
 export default class InputFontSize extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      displayValue: context.fontSize,
+    };
+  }
+
+  handleBlur = () => {
+    const { fontSize } = this.context;
+    this.setState({ displayValue: fontSize });
+  };
+
   handleChange = e => {
     const { updateContext } = this.context;
-    const value = e.target.value ? Number(e.target.value) : '';
-    updateContext({ fontSize: value });
+    const newDisplayValue = e.target.value ? Number(e.target.value) : '';
+
+    let newContextValue = newDisplayValue;
+    if (newContextValue && newContextValue < 12) {
+      newContextValue = 12;
+    } else if (newContextValue && newContextValue > 100) {
+      newContextValue = 100;
+    }
+
+    this.setState({ displayValue: newDisplayValue });
+    updateContext({ fontSize: newContextValue });
   };
 
   render() {
-    const { fontSize } = this.context;
-
+    const { displayValue } = this.state;
     return (
       <div className="form-group">
         <label htmlFor="fontsize">
           Font size (in px)
-          <input type="number" name="fontsize" value={fontSize} onChange={this.handleChange} />
+          <input
+            type="number"
+            name="fontsize"
+            min="12"
+            max="100"
+            value={displayValue}
+            onChange={this.handleChange}
+            onBlur={this.handleBlur}
+          />
+          <input
+            type="range"
+            value={displayValue}
+            min="12"
+            max="100"
+            onChange={this.handleChange}
+            onBlur={this.handleBlur}
+          />
         </label>
       </div>
     );
