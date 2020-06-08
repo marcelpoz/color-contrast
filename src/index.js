55a42c2bab1340cf36a274c0038039543f6f72bd
Marcel <ikbenmarcelpos@gmail.com>
2020-06-08 10:17:10 +0200

    👕 Linted index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './js/App';
 
 const appElement = document.getElementById('app');
 if (!appElement) throw new Error('#app could not be found');
+// eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, appElement);
