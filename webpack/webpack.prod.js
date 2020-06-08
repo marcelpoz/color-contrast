e3e2bf9079def0850fa1b9910fe1222da9ea4084
Marcel <ikbenmarcelpos@gmail.com>
2020-06-08 10:12:32 +0200

    🔧 Added chunk splitting

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,5 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const merge = require('webpack-merge');
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
@@ -8,7 +10,31 @@ const baseConf = require('./webpack.base');
 module.exports = env => {
   return merge(baseConf(env), {
     optimization: {
+      splitChunks: {
+        automaticNameDelimiter: '-',
+        chunks: 'all',
+        maxInitialRequests: Infinity,
+        minSize: 10000,
+        cacheGroups: {
+          vendor: {
+            test: /(node_modules)/,
+            name(module) {
+              return `${module.context
+                .match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
+                .replace('@', '')}`;
+            },
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true,
+          },
+        },
+      },
       minimizer: [
+        new MiniCssExtractPlugin({
+          filename: env.dev ? 'style.css' : 'style-[contenthash:7].css',
+        }),
         new OptimizeCssAssetsPlugin(),
         new TerserPlugin({
           test: /\.(js|jsx|vue)$/,
