e63f2ee4c799118aaa7fc6c67a52783d14b50c2f
Marcel <ikbenmarcelpos@gmail.com>
2020-06-08 10:16:40 +0200

    💅🏻 Styled up App constructor

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
+import { styled } from 'linaria/react';
 
 import { ContextProvider } from './Context';
+import Footer from './components/Footer';
 import Form from './components/Form';
 import Preview from './components/Preview';
 import Results from './components/Results';
+import Root from './components/Root';
+
+const Main = styled.main`
+  display: flex;
+  flex-flow: column nowrap;
+  min-height: 100vh;
+`;
 
 export default () => {
   return (
     <ContextProvider>
-      <main role="main" style={{ display: 'flex', flexFlow: 'column nowrap', minHeight: '100vh' }}>
-        <Form />
-        <Results />
-        <Preview />
-      </main>
+      <Root>
+        <Main>
+          <Form />
+          <Results />
+          <Preview />
+        </Main>
+        <Footer />
+      </Root>
     </ContextProvider>
   );
 };
